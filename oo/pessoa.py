10df64edd2920b700c7b784f83d7df95b15fcfe0
gustavo.moser <gustavo.moser@gmail.com>
2020-05-20 21:56:24 -0300

    Criada Atributo Complexo FIlhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,28 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=39):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    gustavo = Pessoa(nome='Gustavo')
+    luciano = Pessoa(gustavo, nome='Luciano')
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+    p.nome = 'Gustavo'
+    print(p.nome)
+    print(p.idade)
+
+
 
 
 
