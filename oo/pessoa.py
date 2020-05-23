9dc7dce5f86be4ffcec68873012840ee175c4560
gustavo.moser <gustavo.moser@gmail.com>
2020-05-22 21:52:52 -0300

    Update Fase

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,6 +8,17 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return  f'{cls} - olhos {cls.olhos}'
+
+class Homem(Pessoa)
+    pass
+
 if __name__ == '__main__':
     gustavo = Pessoa(nome='Gustavo')
     luciano = Pessoa(gustavo, nome='Luciano')
@@ -30,7 +41,8 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(gustavo.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(gustavo.olhos))
-
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
 
 
 
